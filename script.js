e3033aef8282c5384a96baa804b26998e5e91b0d
Rina Rasia <rinarasia@gmail.com>
2024-02-19 12:56:37 -0800

    Add styling and test collapsible sidebar

---
+++ script.js
@@ -0,0 +1,6 @@
+const logo = document.getElementById('logo');
+const sidebar = document.getElementById('sidebar');
+function toggleSidebar() {
+  sidebar.classList.toggle('closed');
+  console.log(sidebar);
+}
